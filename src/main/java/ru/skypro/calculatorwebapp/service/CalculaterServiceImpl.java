f225bdb9c7b2a4e4a2047e6ba6b091afa1de2c19
Sfa11eN <sfallenn@gmail.com>
2024-05-09 20:07:50 +0300

    First commit

---
+++ src/main/java/ru/skypro/calculatorwebapp/service/CalculaterServiceImpl.java
@@ -0,0 +1,27 @@
+package ru.skypro.calculatorwebapp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class CalculaterServiceImpl  implements CalculatorService{
+    @Override
+    public int sum(int num1, int num2) {
+        return num1+num2;
+    }
+
+    @Override
+    public int substract(int num1, int num2) {
+        return num1-num2;
+    }
+
+    @Override
+    public int multiply(int num1, int num2) {
+        return num1*num2;
+    }
+
+    @Override
+    public float divide(int num1, int num2) {
+        return num1/num2;
+    }
+}
